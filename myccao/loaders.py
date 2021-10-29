1800c69242699682f27dddd636bdeb93b7ef8d56
MattTriano <matt.triano@gmail.com>
2021-10-29 16:46:01 -0500

    Added functions for pulling street shapefiles for Cook County.

--- myccao/loaders.py
+++ myccao/loaders.py
@@ -52,3 +52,31 @@ def get_raw_chicago_city_boundary(
         force_repull=force_repull,
     )
     return gdf
+
+
+def get_raw_cook_county_street_midlines_2015(
+    raw_file_path: Union[str, None] = None, force_repull: bool = False
+) -> gpd.GeoDataFrame:
+    gdf = get_gdf_of_data_portal_data(
+        file_name="cook_county_2015_street_midlines.parquet.gzip",
+        url="https://datacatalog.cookcountyil.gov/api/geospatial/73aw-3v3w?method=export&format=Shapefile",
+        raw_file_path=raw_file_path,
+        force_repull=force_repull,
+    )
+    return gdf
+
+
+def get_raw_cook_county_gis_streets(
+    raw_file_path: Union[str, None] = None, force_repull: bool = False
+) -> gpd.GeoDataFrame:
+    """Data Documentation page:
+    https://hub-cookcountyil.opendata.arcgis.com/datasets/\
+    4569d77e6d004c0ea5fada54640189cf_5/about
+    """
+    gdf = get_gdf_of_data_portal_data(
+        file_name="cook_county_gis_streets.parquet.gzip",
+        url="https://opendata.arcgis.com/api/v3/datasets/4569d77e6d004c0ea5fada54640189cf_5/downloads/data?format=shp&spatialRefId=3435",
+        raw_file_path=raw_file_path,
+        force_repull=force_repull,
+    )
+    return gdf
